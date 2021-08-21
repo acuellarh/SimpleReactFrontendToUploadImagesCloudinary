8d0ed5c934d7019308e6c3ae64db60cbceafdb5f
Alejandro <acuellarh@gmail.com>
2021-08-21 16:29:10 -0500

    Initial Commit with some components for previous class

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './app.style.css'
+
+import {Page} from './components/Page'
+
+
+
+export const App = ()=>{
+
+
+
+  return (
+    <>
+      <Page/>  
+    </>
+   )
+}
+
+
+
+
