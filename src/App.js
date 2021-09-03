cfdf91dbdf3870cb6860d7d7be632eee7b57d853
Alejandro <acuellarh@gmail.com>
2021-09-03 17:49:47 -0500

    Imagenes renderizado de imagenes y carga version de prueba inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import Upload from './pages/Upload';
+import Home from './pages/Home.js';
 
 
 import './app.style.css'
@@ -10,12 +13,32 @@ import {Page} from './components/Page'
 
 export const App = ()=>{
 
-
-
   return (
-    <>
-      <Page/>  
-    </>
+    // <>
+    //   <Page/>  
+    // </>
+
+<div className="container">
+  <Router>
+      <nav className="nav">
+          <div className="nav-brand">Cloudinary Demo</div>
+          <ul className="nav-items">
+              <li className="nav-item">
+                  <Link to="/">Gallery</Link>
+              </li>
+              <li className="nav-item">
+                  <Link to="/upload">Upload</Link>
+              </li>
+          </ul>
+      </nav>
+      <Switch>
+          <Route component={Upload} path="/upload" />
+          <Route component={Home} path="/" />
+      </Switch>
+  </Router>
+
+</div>
+
    )
 }
 
